39e3b1842c2a19fac8611882a09ae8561c1ef33e
Samudra Perera <samudrapup@gmail.com>
2022-07-16 22:36:35 -0400

    completed the basic logic for the JS

--- js/main.js
+++ js/main.js
@@ -3,23 +3,25 @@ const resetGame = document.querySelector('.resetGame')
 const rock = document.querySelector('.rock')
 const paper = document.querySelector('.paper')
 const scissors = document.querySelector('.scissors')
-let playerChoice = [];
+const cScore = document.querySelector('.cScore')
+const pScore = document.querySelector('.pScore')
+
 
 let computer = {
     score: 0,
     choices: ['rock', 'paper', 'scissors'],
-    computerChoiceArray: [],
     computerChoice: function() {
-        this.computerChoiceArray.push(this.choices[Math.floor(Math.random()*3)])
+        return this.choices[Math.floor(Math.random()*3)]
     }
 }
 
 let player = {
     score: 0,
-    playerChoiceArray: [],
+    
+    //Game Start State
     startGame: function() {
-        document.querySelector('.pScore').innerText = `Player Score: ${this.score}`;
-        document.querySelector('.cScore').innerText = `Computer Score: ${computer.score}`;
+        pScore.innerText = `Player Score: ${this.score}`;
+        cScore.innerText = `Computer Score: ${computer.score}`;
         document.querySelector('.startGame').style.display = 'none';
         document.querySelector('.resetGame').style.display = 'flex';
         document.querySelector('.scores').style.visibility = 'visible';
@@ -38,9 +40,9 @@ let player = {
         document.querySelector('.winStatus').style.visibility = 'hidden';
     },
     
-    //Takes in the value of the input from the DOM and pushes into the playerChoiceArray property
+    //Takes in the value of the input from the DOM and returns that value
     playerArray: function(choice) {
-        this.playerChoiceArray.push(choice.value)
+        return choice.value
     }
     
 }
@@ -54,32 +56,49 @@ resetGame.addEventListener('click', function() {
     player.resetGame()
 })
 
+//selecting the player choices and checking the win
 rock.addEventListener('click', function() {
-    player.playerArray(rock)
-    computer.computerChoice()
+    let p = player.playerArray(rock)
+    let c = computer.computerChoice()
+    compareWin(c,p)
 })
 
 paper.addEventListener('click', function() {
-    player.playerArray(paper)
-    computer.computerChoice()
+    let p = player.playerArray(paper)
+    let c = computer.computerChoice()
+    compareWin(c,p)
 })
 
 scissors.addEventListener('click', function() {
-    player.playerArray(scissors)
-    computer.computerChoice()
+    let p = player.playerArray(scissors)
+    let c = computer.computerChoice()
+    compareWin(c,p)
 })
 
 
-/*
-function compareWin() {
-    let len = player.playerChoiceArray.length
-    for(let i = 0; i <= len; i++) {
-        if(player.playerChoiceArray[i] == computer.computerChoiceArray[i]) {
-        
-        }
-    }
+
+
+// Function that takes in the two parameters (the player choice from the DOM element and the computer random choice) and updating score 
+function compareWin(c,p) {
+    if(c == p) {
+        document.querySelector('.winStatus').innerText = `Both players picked ${c} and it is a tie`
+     } else if (p == 'rock' && c == 'scissors') {
+        document.querySelector('.winStatus').innerText = `Player won!`
+        player.score++
+     } else if (p == 'paper' && c == 'rock') {
+        document.querySelector('.winStatus').innerText = `Player won!`
+        player.score++
+     } else if (p == 'scissors' && c == 'paper') {
+        document.querySelector('.winStatus').innerText = `Player won!`
+        player.score++
+     } else {
+        document.querySelector('.winStatus').innerText = `Computer won!`
+        computer.score++
+     }
+     pScore.innerText = `Player Score: ${player.score}`
+     cScore.innerText = `Computer Score: ${computer.score}`
 }
-*/
+
 
 /* 
 Things to accomplish
